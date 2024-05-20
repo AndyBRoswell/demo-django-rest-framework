ee91dea8e818c28dfaabba4ae462f45b82b8fbd1
Andy Bayer Roswell <46857536+AndyBRoswell@users.noreply.github.com>
2024-05-20 20:56:08 +0200

    /

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Snippet # don't use `from tutorial.snippets.models import ...`, or the import of SnippetSerializer will fail [model ... doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS]
+from .models import Snippet  # don't use `from tutorial.snippets.models import ...`, or the import of SnippetSerializer will fail [model ... doesn't declare an explicit app_label and isn't in an application in INSTALLED_APPS]
+from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['url', 'id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'highlight']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -9,7 +9,7 @@ router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing. Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),  # intercepted by built-in auth mechanism
+    # path('', include(router.urls)),  # intercepted by built-in auth mechanism
     path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls'))
 ]
